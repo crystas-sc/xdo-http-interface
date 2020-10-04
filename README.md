892695945939f0cf037d92c18350f7d189d1a982
Christus T <christus.t@outlook.com>
2020-10-04 18:28:31 +0530

    refactoring code

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# xdo_http_interface
+# xdo-http-interface
+Simple HTTP interface for xdotool. 
+Its a keyboard and mouse event  simulation tool for linux. xdotool is binary which does event simulation.
+This project uses Python simple http server to provide an http interface to transmit commands to xdo tool
+
+
+## Requirements
+* Python 3+
+* xdotool 
+* Linux
+
+## Deployment
+```
+python http_xdo.py
+```
+
+## Usage
+* The custom commands can be entered in the inputs and submitted for execution. 
+* Specific commands can be executed by pressing the appropriate buttons as displayed.
+* The yellow box at the bottom can be used as a track pad for execution mouse commands
--- http_xdo.py
+++ http_xdo.py
@@ -7,79 +7,17 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 import urllib
 import subprocess
 import os
+import datetime
 
 
 class S(BaseHTTPRequestHandler):
-	htmlContent = """
-	<html>
-		<body>
-			<form id=form method=get >
-				<p>Last command: %(cmd)s %(key)s </p>
-				
-				<input type=text name="cmd" value="%(cmd)s" />
-				<input type=text name="key" value="%(key)s" />
-				<button type=submit >submit</button>
-				<br/>
-				<div style="display=flex">
-				<button type=button onClick="btnClickKey('--clearmodifiers XF86AudioRaiseVolume')" > Vol+</button>
-				<button type=button onClick="btnClickKey('--clearmodifiers XF86AudioLowerVolume')" > Vol-</button>
-				
-				<button type=button onClick="btnClickKey('alt+Tab')" > alt+Tab</button>
-				<button type=button onClick="btnClickKey('alt+F4')" > alt+F4</button>
-				<button type=button onClick="btnClickMouse(' 0 0 click 1')" > Left Click</button>
-				<button type=button onClick="btnClickMouse(' 0 0 click 3')" > Right Click</button>
-				<button type=button onClick="btnClickSetCmd('mousemove_relative --sync ')" > Mouse Move</button>
-				<button type=button onClick="btnClickSetCmd('key')" > key cmd</button>
-				
-				<button type=button onClick="btnClickKey('--clearmodifiers XF86AudioMute')" > Mute</button>
-				<button type=button onClick="btnClickKey('--clearmodifiers XF86AudioPlay')" > Play</button>
-				<button type=button onClick="btnClickKey('--clearmodifiers XF86AudioStop')" > Stop</button>
-				<button type=button onClick="btnClickKey('--clearmodifiers XF86AudioPrev')" > Prev</button>
-				<button type=button onClick="btnClickKey('--clearmodifiers XF86AudioNext')" > Next</button>
 
-
-				</div>
-				
-				<script>
-				var form = document.getElementById("form");
-				function btnClickKey(value){
-					form.cmd.value= "key";
-					form.key.value=value;
-					form.submit();
-				}
-				function btnClickMouse(value){
-					form.cmd.value= "mousemove_relative --sync ";
-					form.key.value=value;
-					form.submit();
-				}
-				function btnClickSetCmd(cmd){
-					form.cmd.value= cmd;
-					form.key.value="";
-				}
-				</script>
-				<style>
-					button{
-					min-height:70px;
-					min-width:100px;
-					margin:10px
-					
-					}
-				</style>
-			</form>
-		</body>
-	</html>
-	"""
-	
 	def _set_headers(self):
 		self.send_response(200)
 		self.send_header("Content-type", "text/html")
 		self.end_headers()
 
-	def _html(self, message):
-		"""This just generates an HTML document that includes `message`
-		in the body. Override, or re-write this do do more interesting stuff.
-		"""
-		content = f"<html><body><h1>{message}</h1></body></html>"
+	def _html(self, content):
 		return content.encode("utf8")  # NOTE: must return a bytes object!
 
 	def do_GET(self):
@@ -90,11 +28,18 @@ class S(BaseHTTPRequestHandler):
 			queryParams = self.transformQueryParam()
 			key = queryParams["key"]
 			cmd =  queryParams["cmd"]
-			subprocess.call("xdotool "+cmd +" "+ key, shell=True)
+			reqTimestamp =  int(queryParams["timestamp"])
+			currTimestamp =  datetime.datetime.now().replace(tzinfo = datetime.timezone.utc).timestamp()
+			print("timestamp diff ")
+			print((int(currTimestamp) - int(reqTimestamp/1000)))
+			if int(currTimestamp) - int(reqTimestamp/1000) < 3 :
+				print("xdotool "+cmd +" "+ key)
+				subprocess.call("xdotool "+cmd +" "+ key, shell=True)
 			
-		#self.wfile.write(self._html(self.htmlContent%{"cmd":cmd, "key":key}))
 		#self.wfile.write(self.getHtmlContent()%{"cmd":cmd, "key":key})
-		self.wfile.write(self._html(self.getHtmlContent()))
+		formattedString = self.getHtmlContent()
+		formattedString = formattedString.replace("%(cmd)s", cmd).replace("%(key)s", key)
+		self.wfile.write(self._html(formattedString))
 		
 
 	def do_HEAD(self):
@@ -110,10 +55,6 @@ class S(BaseHTTPRequestHandler):
 		print(self.path)
 		queryString = self.path.split("?")[1]
 		queryParams = {item[0]: item[1] for item in urllib.parse.parse_qsl(queryString)}
-		#queryContent = queryString.split("&")
-		#queryParamsArray = [item.split("=") for item in queryContent]
-		#queryParamsArray = [urllib.parse.parse_qs(item) for item in queryContent]
-		#queryParams = {item[0]:item[1] for item in queryParamsArray}
 		print(queryParams)
 		return queryParams
 		
@@ -140,3 +81,4 @@ def run(server_class=HTTPServer, handler_class=S, addr="0.0.0.0", port=80):
 
 if __name__ == "__main__":
 	run()
+
