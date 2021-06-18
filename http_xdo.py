4073ecb53a8cb1ded89b96623a6b8ecd3eb890c6
crystas-sc <christus.t@outlook.com>
2021-06-18 17:16:31 +0530

    File upload and download feature

--- http_xdo.py
+++ http_xdo.py
@@ -9,10 +9,16 @@ import subprocess
 import os
 import datetime
 import ssl
+import os
+from pathlib import Path
+import cgi
+import shutil
 
 
 class S(BaseHTTPRequestHandler):
-
+	base_static_file_path = os.path.dirname(os.path.abspath(__file__))+'/static_files'
+	upload_file_path = os.path.dirname(os.path.abspath(__file__))+'/static_files/uploads'
+	
 	def _set_headers(self):
 		self.send_response(200)
 		self.send_header("Content-type", "text/html")
@@ -21,7 +27,46 @@ class S(BaseHTTPRequestHandler):
 	def _html(self, content):
 		return content.encode("utf8")  # NOTE: must return a bytes object!
 
+	def get_static_file_list(self, path):
+		files = []
+		relative_path = path.replace(self.base_static_file_path, "").strip()
+		if Path(path).is_dir() and path != self.base_static_file_path:
+			dir_list = relative_path.split("/")
+			print(f"dir_list {dir_list}")
+			files.append("/"+"/".join(dir_list[:-1]))
+		files = files + [os.path.join(relative_path, cur_path) for cur_path in os.listdir(path)]
+		return [f"<li><a href='/fileserve/{file}'>{file}</a></li>" for file in files ]
+
 	def do_GET(self):
+		print("Request url {}".format(self.path))
+		
+		if "/fileserve" in self.path:
+			path = self.path
+			file_path = urllib.parse.unquote(path.replace("/fileserve","").strip()).split("/")
+			list_path = self.base_static_file_path
+			if file_path :
+				abs_file_path = os.path.join(self.base_static_file_path, *file_path)
+				if Path(abs_file_path).is_dir() :
+					list_path = os.path.join(self.base_static_file_path, *file_path)
+				elif Path(abs_file_path).is_file() :
+					self.send_response(200)
+					self.send_header("Content-type", "application/octet")
+					self.end_headers()
+					with open(abs_file_path, 'rb') as infile:
+						d = infile.read(1024)
+						while d:
+							self.wfile.write(d)
+							d = infile.read(1024)
+					return
+			print(f"file_path {file_path} base_static_file_path {self.base_static_file_path} list_path {list_path}")
+			
+			file_list_response = "<html><ul>"+''.join(self.get_static_file_list(list_path))+"</ul></html>" 
+			self._set_headers()
+			self.wfile.write(self._html(file_list_response))
+			
+			self.send_response(200)
+			return
+
 		self._set_headers()
 		key =""
 		cmd =""
@@ -41,9 +86,8 @@ class S(BaseHTTPRequestHandler):
 				subprocess.call("xdotool "+cmd +" "+ key, shell=True)
 			
 		#self.wfile.write(self.getHtmlContent()%{"cmd":cmd, "key":key})
-		formattedString = self.getHtmlContent()
-		formattedString = formattedString.replace("%(cmd)s", cmd).replace("%(key)s", key)
-		self.wfile.write(self._html(formattedString))
+		formatted_string = self.getHtmlContent().replace("%(cmd)s", cmd).replace("%(key)s", key)
+		self.wfile.write(self._html(formatted_string))
 		
 
 	def do_HEAD(self):
@@ -51,9 +95,19 @@ class S(BaseHTTPRequestHandler):
 
 
 	def do_POST(self):
-		# Doesn't do anything with posted data
-		self._set_headers()
-		self.wfile.write(self._html("POST!"))
+		if self.path == '/fileupload':
+			form = cgi.FieldStorage(
+                fp=self.rfile,
+                headers=self.headers,
+                environ={'REQUEST_METHOD':'POST',
+                         'CONTENT_TYPE':self.headers['Content-Type'],
+                         })
+			filename = form['file'].filename
+			with open(os.path.join(self.upload_file_path,filename), 'wb') as fh:
+				shutil.copyfileobj(form['file'].file, fh)
+		self.send_response(301)
+		self.send_header('Location','/fileserve/uploads')
+		self.end_headers()
 
 	def transformQueryParam(self):
 		print(self.path)
